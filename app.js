b74d279c893fbb52799ac4108e6c239311993ad2
Samarthya <samarthya55@gmail.com>
2021-11-29 14:33:10 +0530

    basic place routes and error handling

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const placesRoutes = require('./routes/places-routes');
-
+// const usersRoutes = require('./routes/users-route');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }))
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes); // => /api/places/...
+// app.use("/api/users", usersRoutes);
 
-app.listen(5000, () => { console.log('SERVER STARTED ON PORT 5000.'); })
+app.listen(5000, () => { console.log('SERVER STARTED ON PORT 5000.'); });
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,43 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    console.log('GET Request in places');
-    res.json({message: "it works!"})
-})
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the tallest buidings in the world!",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const pid = req.params.pid;
+  const place = DUMMY_PLACES.find((place) => place.id === pid);
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Could not find any place with the provided id." });
+  }
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const uid = req.params.uid;
+  const places = DUMMY_PLACES.filter((place) => place.creator === uid);
+
+  if(!places.length) {
+      return res.status(404).json({message: 'Could not find place for this user.'})
+  }
+
+  res.json({ places });
+});
 
 module.exports = router;
