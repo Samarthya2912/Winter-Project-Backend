f2781d66eeac339ab7b0b4715bb4cb361b06c781
Samarthya <samarthya55@gmail.com>
2021-11-29 20:49:59 +0530

    invalid route handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const HttpError = require("./models/http-error");
 
 const placesRoutes = require("./routes/places-routes");
 // const usersRoutes = require('./routes/users-route');
@@ -9,6 +10,10 @@ app.use(express.json());
 
 app.use("/api/places", placesRoutes); // => /api/places/...
 // app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError('Route not found', 404);
+})
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
