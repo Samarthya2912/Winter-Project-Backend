77c7971421568e773e0b47b97c80fbf46e23b404
Samarthya <samarthya55@gmail.com>
2021-12-07 22:21:40 +0530

    introduced JWT

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,6 +2,8 @@ const HttpError = require("../models/http-error");
 const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const getAllUsers = async (req, res, next) => {
   let users;
@@ -22,10 +24,17 @@ const signup = async (req, res, next) => {
 
   const { name, email, password } = req.body;
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch(err) {
+    return next(new HttpError(err.message, 500));
+  }
+
   let createdUser = new User({
     name,
     email,
-    password,
+    password: hashedPassword,
     places: [],
     image: "https://avatars.githubusercontent.com/u/75770382?s=40&v=4",
   });
@@ -37,7 +46,15 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign({ email: createdUser.email, userId: createdUser.id }, "super_secret", { expiresIn: '1h' });
+  } catch(err) {
+    error = new HttpError(error.message, 404);
+    return next(error);
+  }
+
+  res.status(201).json({ email: createdUser.email, userId: createdUser.id, token });
 };
 
 const login = async (req, res, next) => {
@@ -54,8 +71,17 @@ const login = async (req, res, next) => {
     identifiedUser = await User.findOne({ email });
     if (!identifiedUser) {
       return next(new HttpError("Please signup first", 401));
-    } else if (identifiedUser.password === password) {
-      res.json({ message: "Logged in!!", user: identifiedUser.toObject({ getters: true }) });
+    }
+    
+    let passwordMatched = false;
+    console.log(typeof identifiedUser);
+    passwordMatched = await bcrypt.compare(password, identifiedUser.password);
+    
+    if (passwordMatched) {
+      let token;
+      token = jwt.sign({ email: identifiedUser.email, userId: identifiedUser.id }, "super_secret", { expiresIn: '1h' });
+
+      res.json({ message: "Logged in!!", userId: identifiedUser.id, email: identifiedUser.email, token });
     } else {
       return next(new HttpError("Wrong password", 401));
     }
